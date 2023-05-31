93c1619f8dca0574cbfdf35c0548417b67249377
Ido Kadosh <1234kadosh@gmail.com>
2023-05-31 18:26:46 +0300

    first commit

---
+++ frontend/src/store/store.js
@@ -0,0 +1,18 @@
+import { legacy_createStore as createStore, combineReducers } from 'redux';
+
+import { userReducer } from './user.reducer.js';
+
+const rootReducer = combineReducers({
+	userModule: userReducer,
+});
+
+const middleware = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+	? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+	: undefined;
+export const store = legacy_createStore(rootReducer, middleware);
+
+// store.subscribe(() => {
+// 	console.log('**** Store state changed: ****');
+// 	console.log('storeState:\n', store.getState());
+// 	console.log('*******************************');
+// });
