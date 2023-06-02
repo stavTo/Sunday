32d2b26a8e98b8a3dfe0397f5c654d71870cd208
Stav Tohami <tohami2411@gmail.com>
2023-06-02 12:52:02 +0300

    add label picker - status & priority

--- frontend/src/cmps/task-preview.jsx
+++ frontend/src/cmps/task-preview.jsx
@@ -11,15 +11,15 @@ export function TaskPreview({ task, groupId }) {
 			<TaskTitle groupId={groupId} task={task} />
 			{board.cmpsOrder.map(cmp => {
 				switch (cmp.cmpName) {
-					case 'status-picker':
-					case 'priority-picker':
+					case 'statusPicker':
+					case 'priorityPicker':
 						return <LabelPicker key={cmp.id} groupId={groupId} type={cmp.cmpName} task={task} />
-					case 'date-picker':
+					case 'datePicker':
 						return <DatePicker key={cmp.id} groupId={groupId} task={task} />
-					case 'owner-picker':
-					case 'collaborator-picker':
+					case 'ownerPicker':
+					case 'collaboratorPicker':
 						return <MemberPicker key={cmp.id} type={cmp.cmpName} groupId={groupId} task={task} />
-					case ' date-picker':
+					case ' datePicker':
 						return <DatePicker key={cmp.id} groupId={groupId} task={task} />
 					default:
 						return null
