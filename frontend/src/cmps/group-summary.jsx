1edd3a646932d649d473bdebad7c935634468804
Ido Kadosh <1234kadosh@gmail.com>
2023-06-06 13:22:36 +0300

    fixed some css in group borders

--- frontend/src/cmps/group-summary.jsx
+++ frontend/src/cmps/group-summary.jsx
@@ -1,6 +1,6 @@
 import { object } from 'prop-types'
 import { useSelector } from 'react-redux'
-import { LabelsProgressBar } from './group-summary-calc'
+import { LabelsProgressBar } from './labels-progress-bar'
 
 const STATUS_PICKER = 'statusPicker'
 const PRIORITY_PICKER = 'priorityPicker'
@@ -15,32 +15,45 @@ export function GroupSummary({ group }) {
 	return (
 		<div className="group-summary flex">
 			<div className="empty-margin-footer"></div>
-			<div className="empty-owner-container group-summary-data"></div>
+
 			{board.cmpsOrder.map(cmp => {
 				switch (cmp.cmpName) {
 					case STATUS_PICKER:
 					case PRIORITY_PICKER:
 						return (
 							<div key={cmp.id} className="group-summary-data">
-								<LabelsProgressBar group={group} type={cmp.cmpName} board={board} />
+								<LabelsProgressBar
+									defaultWidth={cmp.defaultWidth}
+									group={group}
+									type={cmp.cmpName}
+									board={board}
+								/>
 							</div>
+						)
+					case OWNER_PICKER:
+						return (
+							<div
+								key={cmp.id}
+								style={{ width: cmp.defaultWidth }}
+								className="empty-owner-container group-summary-data"
+							></div>
 						)
 					case DATE_PICKER:
 						return (
 							<div key={cmp.id} className="group-summary-data">
-								<div>{cmp.cmpName}</div>
+								<div style={{ width: cmp.defaultWidth }}>{cmp.cmpName}</div>
 							</div>
 						)
 					case COLLABORATOR_PICKER:
 						return (
 							<div key={cmp.id} className="group-summary-data">
-								<div>{cmp.cmpName}</div>
+								<div style={{ width: cmp.defaultWidth }}>{cmp.cmpName}</div>
 							</div>
 						)
 					case TIMELINE_PICKER:
 						return (
 							<div key={cmp.id} className="group-summary-data">
-								<div>{cmp.cmpName}</div>
+								<div style={{ width: cmp.defaultWidth }}>{cmp.cmpName}</div>
 							</div>
 						)
 					default:
---
+++ frontend/src/cmps/labels-progress-bar.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react'
+import { TippyContainer } from './tippy-container'
+import { utilService } from '../services/util.service'
+
+export function LabelsProgressBar({ board, group, type, defaultWidth }) {
+	const [labelsName, setLabelsName] = useState('')
+	const [labelsNameInBoard, setLabelsNameInBoard] = useState('')
+
+	useEffect(() => {
+		if (type === 'statusPicker') {
+			setLabelsName('status')
+			setLabelsNameInBoard('statusLabels')
+		} else if (type === 'priorityPicker') {
+			setLabelsName('priority')
+			setLabelsNameInBoard('priorityLabels')
+		}
+	}, [type])
+
+	function getLabelColor(labelName) {
+		const labels = board[labelsNameInBoard]
+		const label = labels.find(l => l.title === labelName)
+		return label.color
+	}
+
+	function getLabelRatio() {
+		const mapCount = calcLabels()
+		const labelNames = Object.keys(mapCount)
+		const values = Object.values(mapCount)
+		const sum = values.reduce((acc, val) => acc + val, 0)
+		const valuesPercent = values.map(val => {
+			if (val === 0) return
+			return `${val}/${sum}`
+		})
+
+		return labelNames.map((label, idx) => ({
+			[label]: valuesPercent[idx],
+		}))
+	}
+
+	function calcLabels() {
+		return group.tasks.reduce((acc, task) => {
+			const status = task[labelsName]
+			if (acc[status]) acc[status] += 1
+			else acc[status] = 1
+			return acc
+		}, {})
+	}
+
+	if (!labelsNameInBoard) return
+
+	return (
+		<div className="labels-progress-bar" style={{ width: defaultWidth }}>
+			<ul className="list-labels-progress-bar flex">
+				{getLabelRatio().map((val, idx) => {
+					const label = Object.keys(val)[0]
+					const widthFraction = val[label]
+					const widthPercent = utilService.fractionToPercent(widthFraction).toFixed(1)
+					return (
+						<TippyContainer key={idx} txt={`${label} ${widthFraction} ${widthPercent}%`}>
+							<li
+								className="progress-bar-item"
+								style={{
+									backgroundColor: getLabelColor(label),
+									width: widthPercent + '%',
+								}}
+							></li>
+						</TippyContainer>
+					)
+				})}
+			</ul>
+		</div>
+	)
+}
