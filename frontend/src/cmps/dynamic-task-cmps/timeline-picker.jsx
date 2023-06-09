c48cb81b67f9c489f8d79d44a5cee7769ab2509b
Ido Kadosh <1234kadosh@gmail.com>
2023-06-09 14:01:01 +0300

    fixed some more z-index issues

--- frontend/src/cmps/dynamic-task-cmps/timeline-picker.jsx
+++ frontend/src/cmps/dynamic-task-cmps/timeline-picker.jsx
@@ -233,7 +233,7 @@ export function TimelinePicker({ task, groupId, defaultWidth }) {
 						style={styles.popper}
 						{...attributes.popper}
 					>
-						<div className="modal-up-arrow" ref={setArrowElement} style={styles.arrow}></div>
+						<div className="modal-up-arrow timeline-arrow" ref={setArrowElement} style={styles.arrow}></div>
 						<DayPicker
 							numberOfMonths={2}
 							mode="range"
